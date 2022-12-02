df66e1996b9777534d2629bb629bae85bef31af3
sambathdev <46620661+sambathdev@users.noreply.github.com>
2022-12-02 23:39:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3001
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Exelent, Perfect. The dream')
+  res.send('Hello World! Exelent, Perfect. The dream. Let go.')
 })
 
 app.listen(port, () => {
